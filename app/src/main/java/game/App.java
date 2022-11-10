e730375b844ad08f3acd934760f5977273167a06
Asimina Athanasopoulou <a.athanasopoulou@elsevier.com>
2022-11-10 20:07:34 +0000

    Commiting code updates in part_1

--- app/src/main/java/game/App.java
+++ app/src/main/java/game/App.java
@@ -11,27 +11,31 @@ import com.google.common.collect.Lists;
 public class App {
     private static Random rand = new Random();
     private static Scanner input = new Scanner(System.in);
-    // public static List<String> words = new ArrayList<>();
-    private static Scanner reader = null;
+    public static List<String> words = Lists.newArrayList("dog", "cat", "lion", "tiger", "bear", "frog", "whale", "monkey", "bird", "fish");
+
+    public App(List<String> words) {
+        this.words = words;
+    }
 
     private static int playGame(List<String> words) {
         int score = 5;
         return score;
+    } 
+    
+    static String getRandomWord(){
+        Random r = new Random();
+        int randomWord = r.nextInt(words.size());
+        String word = words.get(randomWord);
+
+        return word;
     }
-
-        List<String> words = Lists.newArrayList("dog", "cat", "lion", "tiger", "bear", "frog", "whale", "monkey", "bird", "fish");
-            
         
-    public App(List<String> words) {
-            this.words = words;
-        }
-
-
+    
     public static void main(String[] args) {
-        while(reader.hasNextLine()) {
-            String word = reader.nextLine();
-            words.add(word);
-        }
+        // while(reader.hasNextLine()) {
+        //     String word = reader.nextLine();
+        //     words.add(word);
+        // }
 
         int score = playGame(words);
 
